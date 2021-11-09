ab0e282da43ad72fe6fbcd41add9a3500250b4f8
OxyProgram <75229680+OxyProgram@users.noreply.github.com>
2021-11-10 01:11:40 +0200

    pridedami src failai

---
+++ src.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <random>
+#include <chrono>
+#include <chrono>
+#include <ratio>
+#include <thread>
+#include "header.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    Hash hash;
+    string input = *argv;
+    string hashString = hash.generateHash(input);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
